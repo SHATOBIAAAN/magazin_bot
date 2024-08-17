471ffdb0068a86c5e4588d6f828c1afe5efc0f34
SHATOBIAAAN <shatobinsergei06@gmail.com>
2024-08-17 22:20:45 +0300

    magazin

---
+++ main.py
@@ -0,0 +1,52 @@
+import colorama
+import asyncio
+import logging
+
+from aiogram import executor, Dispatcher
+from tgbot.handlers import dp
+from tgbot.middlewares import setup_middlewares
+from tgbot.data.config import db
+from tgbot.data.loader import scheduler
+from tgbot.utils.other_functions import update_profit_week, update_profit_day, autobackup_db
+from tgbot.utils.utils_functions import  check_contests, check_rates
+
+logging.basicConfig(format=u'%(filename)s [LINE:%(lineno)d] #%(levelname)-8s [%(asctime)s]  %(message)s', level=logging.INFO)
+colorama.init()
+
+
+# Запуск заданий
+async def scheduler_start():
+    scheduler.add_job(update_profit_week, "cron", day_of_week="mon", hour=00)
+    scheduler.add_job(update_profit_day, "cron", hour=00)
+    scheduler.add_job(autobackup_db, "cron", hour=00)
+   
+    scheduler.add_job(check_rates, 'cron', hour=00)
+
+
+# Выполнение функции после запуска бота
+async def on_startup(dp: Dispatcher):
+    await scheduler_start()
+
+    setup_middlewares(dp)
+
+
+
+
+# Выполнение функции после выключения бота
+async def on_shutdown(dp: Dispatcher):
+
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+    await (await dp.bot.get_session()).close()
+
+
+if __name__ == "__main__":
+    scheduler.start()
+    loop = asyncio.get_event_loop()
+
+    loop.create_task(db.create_db())
+    loop.create_task(check_rates())
+    loop.create_task(check_contests())
+   
+
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown, skip_updates=True)
